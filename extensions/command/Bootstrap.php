2579cf6861aa1fb3782828d043dfa39a80d6d022
Tom Maiaroto <tom@shift8creative.com>
2012-08-08 17:09:41 -0700

    command updates

--- extensions/command/Bootstrap.php
+++ extensions/command/Bootstrap.php
@@ -71,6 +71,23 @@ class Bootstrap extends \lithium\console\Command {
 		}
 	}
 	
+	/**
+	 * Installs any dependencies for the package/library via git.
+	 * 
+	 * Each library built specifically for use with Lithium Bootstrap
+	 * (also known as "package" or "plugin") may require other libraries
+	 * in order to work. These dependencies may or may not be other Lithium
+	 * Bootstrap packages. They could simply be any library that's available
+	 * from a git repository. However, they must come from a git repo.
+	 * 
+	 * This method will clone all of the dependencies into the main app's
+	 * libraries directory. It will skip over any that may already exist.
+	 * Note: It is possible that, despite having a library already, the
+	 * dependency shares a name with another library. This would create
+	 * a conflict for now and would need to be manually addressed.
+	 * 
+	 * @param string $packageName The package/library name from a Lithium Bootstrap plugin repository.
+	 */
 	public static function installDependencies($packageName=null) {
 		if(empty($packageName)) {
 			echo "No package name provided." . PHP_EOL;
@@ -93,19 +110,46 @@ class Bootstrap extends \lithium\console\Command {
 				// system("/usr/bin/env -i HOME={$appRoot} {$git} {$command} 2>&1");
 				// So instead, clone them.
 				
-				if(file_exists('libraries/' . $lib)) {
+				if(file_exists($appRoot . '/libraries/' . $lib)) {
 					// TODO: Read .git/config files and check for this.
 					echo "It seems that {$lib} already exists. Please ensure that is it compatible with or is:\n {$repo}\n";
 				} else {
 					$command = 'clone ' . $repo . ' libraries/' . $lib;
 					system("/usr/bin/env -i HOME={$appRoot} {$git} {$command} 2>&1");
+					// Hey, this library may have submodules of its own...Get them.
+					$packageRoot = $appRoot . '/libraries/' . $packageName;
+					$command = 'submodule update --init --recursive';
+					system("/usr/bin/env -i HOME={$packageRoot} {$git} {$command} 2>&1");
 				}
 			}
-			// Don't need this anymore. Going to leave it commented out because I can use this elsewhere.
-			//system("/usr/bin/env -i HOME={$appRoot} {$git} submodule init --update --recursive 2>&1");
+			echo "\nAll dependencies have been installed.\n";
 			echo PHP_EOL;
 		}
 		
+	}
+	
+	/**
+	 * Configures a package/library for Lithium Bootstrap.
+	 * 
+	 * If a library is built specifically for Lithium Bootstrap, it can have
+	 * a `config.ini` file bundled with it under its `config` directory.
+	 * This confg file holds instructions for us in order to make additional
+	 * configurations that may (or may not) be necessary before using it.
+	 * 
+	 * This is anything that can't be covered by simply cloning the library
+	 * into the main application and getting any submodules. This also does
+	 * not include dependency management. For installation of dependencies,
+	 * see the `installDependencies` command.
+	 * 
+	 * @param string $packageName The package/library name from a Lithium Bootstrap plugin repository.
+	 */
+	public static function configurePackage($packageName=null) {
+		// TODO: move some code from install() down here.
+		// This allows libraries to be configured again without going through
+		// the entire installation process again (which has checks to not clone
+		// if directories already exist, etc.) which makes more sense if a user
+		// wants to "re" - configure the package.
+		// This also may become a sort of wizard...
 	}
 	
 	private static function _getPackageConfig($packageName=null) {
