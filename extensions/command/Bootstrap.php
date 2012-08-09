2791d524e4d71fc994b19f91ef3d46b3c3beb692
Tom Maiaroto <tom@shift8creative.com>
2012-08-08 20:09:42 -0700

    allowing bootstrap to install from repo files

--- extensions/command/Bootstrap.php
+++ extensions/command/Bootstrap.php
@@ -13,11 +13,30 @@ use DirectoryIterator;
 
 class Bootstrap extends \lithium\console\Command {
 	
-	private static $_packageConfig;
-
 	/**
-	 * Generates the appropriates indexes for the words collection.
+	 * Holds a package/library configuration.
+	 * 
+	 * @var array
+	 */
+	private static $_packageConfig;
+	
+	/**
+	 * A list of all packages (libraries) and their repository locations.
+	 * 
+	 * @var array
+	 */
+	private static $_packages;
+	
+	/**
+	 * Installs a package/library from the Lithium Bootstrap repository.
 	 *
+	 * Note: Additional repository coinfiguration files can be added
+	 * to the main application under the `_repos` directory.
+	 * There is also a default, core list under the `li3b_core` library's
+	 * `_repos` directory.
+	 * 
+	 * For now, all packages/libraries must be uniquely named.
+	 * 
 	 * @param $packageName The name of the library/plugin to install.
 	 * @return void
 	*/
@@ -27,12 +46,33 @@ class Bootstrap extends \lithium\console\Command {
 			exit();
 		}
 		
-		Bootstrap::installDependencies($packageName);
+		// Get all of the packages from the repo ini files.
+		// See if this package even exists.
+		static::_collectPackages();
+		if(!in_array($packageName, array_keys(static::$_packages))) {
+			echo "No package found by that name." . PHP_EOL;
+			exit();
+		}
 		
-		$coreConfig = Libraries::get('li3b_core');
 		$appConfig = Libraries::get(true);
 		$appRoot = $appConfig['path'];
 		$appWebroot = $appRoot . '/webroot';
+		
+		$command = 'clone ' . static::$_packages[$packageName] . ' libraries/' . $packageName;
+		system("/usr/bin/env -i HOME={$appRoot} {$git} {$command} 2>&1");
+		// Hey, this library may have submodules of its own...Get them.
+		$packageRoot = $appRoot . '/libraries/' . $packageName;
+		$command = 'submodule update --init --recursive';
+		system("/usr/bin/env -i HOME={$appRoot} {$git} {$command} 2>&1");
+		
+		if(!file_exists($appRoot . '/libraries/' . $packageName)) {
+			echo "Failed to retrieve the package/library." . PHP_EOL;
+			exit();
+		}
+		
+		Bootstrap::installDependencies($packageName);
+		
+		$coreConfig = Libraries::get('li3b_core');
 		$packageWebroot = $appRoot . '/libraries/' . $packageName . '/webroot';
 		$libraryAddFile = $appRoot . '/config/bootstrap/libraries/' . $packageName . '.php';
 		$configOptions = isset(static::$_packageConfig['configuration']) ? static::$_packageConfig['configuration']:array();
@@ -94,6 +134,14 @@ class Bootstrap extends \lithium\console\Command {
 			exit();
 		}
 		
+		// Get all of the packages from the repo ini files.
+		// See if this package even exists.
+		static::_collectPackages();
+		if(!in_array($packageName, array_keys(static::$_packages))) {
+			echo "No package found by that name." . PHP_EOL;
+			exit();
+		}
+		
 		static::_getPackageConfig($packageName);
 		
 		$appConfig = Libraries::get(true);
@@ -152,6 +200,25 @@ class Bootstrap extends \lithium\console\Command {
 		// This also may become a sort of wizard...
 	}
 	
+	
+	/**
+	 * Searches the repositories for plugins.
+	 * 
+	 */
+	public static function search($query=null) {
+	}
+	
+	/**
+	 * Gets a package's configuration.
+	 * 
+	 * This let's us know about its dependencies and such
+	 * so that they can be installed. This can only be
+	 * ran once the package has been cloned. The config file
+	 * is contained within the library/package's codebase.
+	 * 
+	 * @param string $packageName
+	 * @return boolean
+	 */
 	private static function _getPackageConfig($packageName=null) {
 		$appConfig = Libraries::get(true);
 		$appRoot = $appConfig['path'];
@@ -173,6 +240,55 @@ class Bootstrap extends \lithium\console\Command {
 		}
 	}
 	
+	/**
+	 * Loops all Lithium Bootstrap repository files and puts together
+	 * a list of all packages and their repository locations.
+	 * 
+	 * Note: This will NOT include any additional information such as
+	 * description and friendly names. This is simply used to get
+	 * a package/plugin from a git repository.
+	 * 
+	 * Also note: Package names MUST be unique.
+	 * It's a good idea, if you're making one, to name it something
+	 * creative that you know will end up being unique. Otherwise,
+	 * it may not be accessible.
+	 * 
+	 * TODO: Work on some sort of prefix convention for each repo ini
+	 * that will avoid this issue. So that only each ini file must
+	 * contain unique names for packages.
+	 * 
+	 */
+	private static function _collectPackages() {
+		$appConfig = Libraries::get(true);
+		$appRoot = $appConfig['path'];
+		$appRepoPath = $appRoot . '/_repos';
+		$coreRepoPath = $appRoot . '/libraries/li3b_core/_repos';
+		
+		$packageList = array();
+		// Add packages from li3b_core.
+		foreach(glob($coreRepoPath . '/*.ini') as $repoFile) {
+			$packages = parse_ini_file($repoFile);
+			foreach($packages as $package => $config) {
+				if(isset($config['repo'])) {
+					$packageList[$package] = $config['repo'];
+				}
+			}
+		}
+		
+		// Add packages from any repos that the main application may have added.
+		foreach(glob($appRepoPath . '/*.ini') as $repoFile) {
+			$packages = parse_ini_file($repoFile);
+			foreach($packages as $package => $config) {
+				// If the repo key was set and if we don't already have this package.
+				if(isset($config['repo']) && !in_array($package, array_keys($packageList))) {
+					$packageList[$package] = $config['repo'];
+				}
+			}
+		}
+		
+		static::$_packages = $packageList;
+		
+	}
 	
 }
 ?>
